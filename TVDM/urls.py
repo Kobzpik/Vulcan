e19f141e7d020fa4e6aabcc219e0d36fdbbe3dae
Kobzpik <prabath.pik2299@gmail.com>
2021-12-27 22:26:15 +0530

    add html file load with route

--- TVDM/urls.py
+++ TVDM/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from .import views
+from .views import testRoute
 
 urlpatterns = [
     path('',views.home,name="home"),
+    path('route/',testRoute,name="route"),
+
    
 ]
--- TVDM/views.py
+++ TVDM/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
         return render(request, 'index.htm')
+
+
+def testRoute(request):
+        return render(request, 'route.htm')
