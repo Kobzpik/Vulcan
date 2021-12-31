3d8384c0a8368b78d67f0eb2974102e6372df25d
Kobzpik <prabath.pik2299@gmail.com>
2021-12-31 20:02:37 +0530

    testing retrive data from database

--- TVDM/models.py
+++ TVDM/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class list(models.Model):
+    Id=models.AutoField(primary_key=True)
+    firstName= models.CharField(max_length=30)
+    age=models.CharField(max_length=10)
+    class Meta:
+        db_table="beta"
+    
+    def __str__(self):
+         return self.item
+    
+
+   
