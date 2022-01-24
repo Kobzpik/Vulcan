cba5f7b51c6e3bb50259a0626f40e9d7130c4b04
Kobzpik <prabath.pik2299@gmail.com>
2022-01-24 20:10:15 +0530

    add digital fine paper

--- officer/models.py
+++ officer/models.py
@@ -1,3 +1,31 @@
+from xml.parsers.expat import model
 from django.db import models
 
 # Create your models here.
+class District(models.Model):
+    name = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.name
+
+
+class Location(models.Model):
+    district = models.ForeignKey(District, on_delete=models.CASCADE)
+    name = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.name
+
+
+class Fine(models.Model):
+    id=models.AutoField(primary_key=True)
+    driver_Name = models.CharField(max_length=50)
+    vehicle_No = models.CharField(max_length=50)
+    driver_license_No = models.CharField(max_length=124)
+    district = models.ForeignKey(District, on_delete=models.SET_NULL, blank=True, null=True)
+    location = models.ForeignKey(Location, on_delete=models.SET_NULL, blank=True, null=True)
+    Police_station = models.CharField(max_length=130)
+    Issuing_officer_ID = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.driver_Name
--- officer/views.py
+++ officer/views.py
@@ -1,5 +1,35 @@
 from django.shortcuts import render
+from django.shortcuts import redirect, get_object_or_404
+from .forms import FineCreationForm
+from .models import Fine, Location
 
-# Create your views here.
-def officer_index(request):
-    return render(request, 'officer/officer_index.htm')
+
+def fine_create_view(request):
+    form = FineCreationForm()
+    if request.method == 'POST':
+        form = FineCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('fine_add')
+    return render(request, 'officer/officer_index.htm', {'form': form})
+
+
+def fine_update_view(request, pk):
+    fine = get_object_or_404(Fine, pk=pk)
+    form = FineCreationForm(instance=fine)
+    if request.method == 'POST':
+        form = FineCreationForm(request.POST, instance=fine)
+        if form.is_valid():
+            form.save()
+            return redirect('fine_change', pk=pk)
+    return render(request, 'officer/officer_index.htm', {'form': form})
+
+
+# AJAX
+def load_locations(request):
+    district_id = request.GET.get('district_id')
+    locations = Location.objects.filter(district_id=district_id).all()
+    return render(request, 'officer/lcoation_list_options.htm', {'locations': locations})
+    #return JsonResponse(list(cities.values('id', 'name')), safe=False)
+
+  
