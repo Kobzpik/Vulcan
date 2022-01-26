357b3ccdcd61ef8f9013f362ec435cd7b3e6f548
Kobzpik <prabath.pik2299@gmail.com>
2022-01-26 17:28:03 +0530

    modified fine paper

--- officer/models.py
+++ officer/models.py
@@ -1,5 +1,7 @@
+from sqlite3 import Date
 from xml.parsers.expat import model
 from django.db import models
+from authenticate.models import Officer
 
 # Create your models here.
 class District(models.Model):
@@ -16,6 +18,11 @@ class Location(models.Model):
     def __str__(self):
         return self.name
 
+class Offence(models.Model):
+    offence = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.offence
 
 class Fine(models.Model):
     id=models.AutoField(primary_key=True)
@@ -24,8 +31,11 @@ class Fine(models.Model):
     driver_license_No = models.CharField(max_length=124)
     district = models.ForeignKey(District, on_delete=models.SET_NULL, blank=True, null=True)
     location = models.ForeignKey(Location, on_delete=models.SET_NULL, blank=True, null=True)
+    Nature_of_Offence = models.ForeignKey(Offence, on_delete=models.SET_NULL, blank=True, null=True)
+    date = models.DateField()
+    time = models.TimeField() 
     Police_station = models.CharField(max_length=130)
-    Issuing_officer_ID = models.CharField(max_length=20)
+    Issuing_officer_Name= models.CharField(max_length=20)
 
     def __str__(self):
         return self.driver_Name
