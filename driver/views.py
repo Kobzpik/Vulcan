5a30d9e408e92ae081c17ce0476b3fabe9b30f87
Kobzpik <prabath.pik2299@gmail.com>
2022-02-14 22:23:24 +0530

    add driver profile

--- driver/views.py
+++ driver/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,get_object_or_404
 from officer.models import Fine
-from authenticate.models import User
+from authenticate.models import Driver
 
 
 # Create your views here.
@@ -8,5 +8,6 @@ def index(request):
         return render(request, 'driver/index_driver.htm')
         
 def fine_list(request,pk):
-        fined = Fine.objects.filter(driver_id = pk)
-        return render(request, 'driver/fines_list.htm',{'fined': fined})
+        fined = Fine.objects.filter(driver_id = pk)     
+        finedh = Driver.objects.get(user_id = pk)
+        return render(request, 'driver/fines_list.htm',{'fined': fined,'finedh':finedh})
